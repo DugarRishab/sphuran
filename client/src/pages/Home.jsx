bbb89a2277a46430be2a14aacd86a324d4548ca6
Rishab Dugar <www.anitadugar9163@gmail.com>
2023-03-09 17:39:17 +0530

    added home page header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import CustomButton from '../components/CustomButton/CustomButton';
+import "./style.css";
 
 const Home = () => {
-	return ( 
-		<div class="home">
-			<h1></h1>
+	return (
+		<div className="home">
+			<div className="header">
+				<div className="date">07_02_2023 - 09_02_2023</div>
+				<div className="title">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				</div>
+				<CustomButton text="Register Now" variant="contained"></CustomButton>
+				<div className="logos">
+					<div className="item">
+						<img src="/assets/iiest-logo-white.png" alt="" />
+					</div>
+					<div className="divider"></div>
+					<div className="item">
+						<img src="/assets/iiest-logo-white.png" alt="" />
+					</div>
+				</div>
+			</div>
 		</div>
-	 );
+	);
 }
  
 export default Home;
