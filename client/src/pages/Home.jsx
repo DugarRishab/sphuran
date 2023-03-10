a8579713eb8507142ae90d4605ec2046b90ec057
artemis-i-guess <nafis30503@gmail.com>
2023-03-10 18:40:20 +0530

    made grid and tagline 2 by nafis

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -87,64 +87,34 @@ const Home = () => {
 				<div className="gallery-grid">
 					<p>Gallery</p>
 
-							<section class="layout">
-								<div class="L">1</div>
-								<div class="S1">2</div>
-								<div class="S2">3</div>
-								<div class="S3">4</div>
-								<div class="S4">5</div>
-								<div class="S5">6</div>
-								<div class="S6">7</div>
-								<div class="L2">8</div>
-								<div class="S7">9</div>
-							</section>
-					
 					<div class="grid-container">
-						<div class="grid-item">
-						<img
-							class="grid-item-1"
-							src="/assets/0__7RPcbo39crq3lgq 2.png"
-							alt=""
-					/>	
-						</div>
-						<div class="grid-item">
-						<img
-							class="grid-item-2"
-							src="/assets/0__7RPcbo39crq3lgq 2.png"
-							alt=""
-					/>
-						</div>
-						<div class="grid-item">
-						<img
-							class="grid-item-3"
-							src="/assets/0__7RPcbo39crq3lgq 2.png"
-							alt=""
-					/>
-						</div>
-						<div class="grid-item">
-						<img
-							class="grid-item-4"
-							src="/assets/0__7RPcbo39crq3lgq 2.png"
-							alt=""
-					/>
-						</div>
-						<div class="grid-item">
-						<img
-							class="grid-item-5"
-							src="/assets/0__7RPcbo39crq3lgq 2.png"
-							alt=""
-					/>
-						</div>
-						<div class="grid-item">
-							<img
-							class="grid-item-6"
-							src="/assets/0__7RPcbo39crq3lgq 2.png"
-							alt=""
-					/>
-						</div>
-						
-					</div>
+								<div class="L"><img class="image1" src="/assets/0__7RPcbo39crq3lgq 2.png" alt=""/>
+								</div>
+								<div class="S1"><img class="image1" src="/assets/0__7RPcbo39crq3lgq 2.png" alt=""/>
+								</div>
+								<div class="S2"><img class="image1" src="/assets/0__7RPcbo39crq3lgq 2.png" alt=""/>
+								</div>
+								<div class="S3"><img class="image1" src="/assets/0__7RPcbo39crq3lgq 2.png" alt=""/>
+								</div>
+								<div class="S4"><img class="image1" src="/assets/0__7RPcbo39crq3lgq 2.png" alt=""/>
+								</div>
+								<div class="S5"><img class="image1" src="/assets/0__7RPcbo39crq3lgq 2.png" alt=""/>
+								</div>
+								<div class="S6"><img class="image1" src="/assets/0__7RPcbo39crq3lgq 2.png" alt=""/>
+								</div>
+								<div class="L2"><img class="image1" src="/assets/0__7RPcbo39crq3lgq 2.png" alt=""/>
+								</div>
+								<div class="S7"><img class="image1" src="/assets/0__7RPcbo39crq3lgq 2.png" alt=""/>
+								</div>
+								<div class="S8"><img class="image1" src="/assets/0__7RPcbo39crq3lgq 2.png" alt=""/>
+								</div>
+							</div>
 
+				</div>
+
+				<div className="tagline-2">
+					<h2>lorem ipsum lorem ipsum lorem ipsum lorem ipsum</h2>
+					<h4 className="author">-lorem</h4>
 				</div>
 
 			</div>
@@ -153,11 +123,3 @@ const Home = () => {
 };
 
 export default Home;
-
-/* 
-	<h2>Gallery </h1>
-	<div className = "gallery"> 
-		
-	</div>
-
-*/
