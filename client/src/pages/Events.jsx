897e30d97d069d86dd1e1b3198af87a10eb20412
artemis-i-guess <nafis30503@gmail.com>
2023-03-11 14:37:07 +0530

    search cards made, and tgline added by nafis

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -3,7 +3,36 @@ import React from 'react';
 const Events = () => {
 	return ( 
 		<div className="events">
-			// do shit here
+			<div className="header">Events</div>
+
+			<div className="lower-body">
+				<div className="search-results">
+					<div className="search-card">
+						<div className="image-container">
+							<img class="image1" src="/assets/0__7RPcbo39crq3lgq 2.png" alt=""/>
+						</div>
+						<div className="card-text">
+							<p className="text-head">lorem ipsum</p>
+							lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum 
+							lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum 
+							lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum 
+							lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum 
+						</div>
+						<div className="read-more">Read more</div>
+					</div>
+					<div className="search-card">
+						
+					</div>
+					<div className="search-card">
+						
+					</div>
+				</div>
+				<div className="tagline-2">
+					<h2>lorem ipsum lorem ipsum lorem ipsum lorem ipsum</h2>
+					<h4 className="author">-lorem</h4>
+				</div>
+			</div>
+
 			</div>
 	 );
 }
