e097993aaf542f934e9e435f40e20ff1fbb43e66
Rishab Dugar <www.anitadugar9163@gmail.com>
2023-03-24 12:22:00 +0530

    updated home page links

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import CustomButton from "../components/CustomButton/CustomButton";
 import "./style.css";
+import {Link} from "react-router-dom"
 
 const Home = () => {
 	const [about, setAbout] = useState("sphuran")
@@ -9,12 +10,16 @@ const Home = () => {
 			<div className="header">
 				<div className="date">07_04_2023 - 09_04_2023</div>
 				<div className="title">
-				The Annual Technical Fest of the Department of Electrical Engineering
+					The Annual Technical Fest of the Department of Electrical
+					Engineering
 				</div>
-				<CustomButton
-					text="Register Now"
-					variant="contained"
-				></CustomButton>
+				<Link to="/register">
+					<CustomButton
+						text="Register Now"
+						variant="contained"
+					></CustomButton>
+				</Link>
+
 				<div className="logos">
 					<div className="item">
 						<img src="/assets/EES_edited.png" alt="" />
@@ -62,51 +67,113 @@ const Home = () => {
 								? "Indian Institute of Engineering Science and Technology, Shibpur (IIEST Shibpur) is a premier technical institute located in the historic city of Howrah, West Bengal, India. With a rich legacy dating back to 1856, IIEST Shibpur offers a wide range of undergraduate and postgraduate programs in engineering, technology, and basic sciences. The institute is renowned for its world-class faculty, state-of-the-art infrastructure, and innovative research culture that nurtures the next generation of engineers, scientists, and technologists."
 								: "The Electrical Engineers' Society (EES) at IIEST Shibpur is a student-run org that cultivates a community of electrical ELECTRICAL engineers. EES offers opportunities to showcase skills, collaborate on projects, and learn about advancements in electrical engineering through events, guest lectures, and workshops. EES plays a crucial role in supplementing students' education and preparing them for successful careers."}
 
-							<p className="link">{ about === "iiests" ? "Check our IIESTS website" : about === "sphuran" ? "Check out our events" : "Check out EES website"}</p>
+							<p className="link">
+								{about === "iiests" ? (
+									<a href="https://www.iiests.ac.in">
+										Check out our IIESTS website
+									</a>
+								) : about === "sphuran" ? (
+									<Link to="/events">
+										Check out our events
+									</Link>
+								) : (
+									<a href="https://www.eesiiests.in">
+										Check out our EES website
+									</a>
+								)}
+							</p>
 						</div>
 					</div>
 				</div>
 
 				<div className="tagline">
-					<h2>Experience the Spark of Innovation: A Celebration of Electrical Excellence</h2>
-					<h4 className="author"></h4>
+					<h4 className="author">
+						Experience the Spark of Innovation:
+					</h4>
+					<h2>A Celebration of Electrical Excellence</h2>
 				</div>
-
-				
 
 				<div className="gallery-grid">
 					<p>Gallery</p>
 
 					<div class="grid-container">
-								<div class="L"><img class="image1" src="/assets/gallery/69841845_131138014931266_2812637652514242560_n.jpg" alt=""/>
-								</div>
-								<div class="S1"><img class="image1" src="/assets/gallery/70122500_121337579244643_3821815105759215616_n.jpg" alt=""/>
-								</div>
-								<div class="S2"><img class="image1" src="/assets/gallery/70922147_131138091597925_598385121535459328_n.jpg" alt=""/>
-								</div>
-								<div class="S3"><img class="image1" src="/assets/gallery/71813259_131137258264675_209588408270454784_n.jpg" alt=""/>
-								</div>
-								<div class="S4"><img class="image1" src="/assets/gallery/71766217_131137268264674_3070247042149253120_n.jpg" alt=""/>
-								</div>
-								<div class="S5"><img class="image1" src="/assets/gallery/71213662_131137248264676_505020924886515712_n.jpg" alt=""/>
-								</div>
-								<div class="S6"><img class="image1" src="/assets/gallery/70122500_121337579244643_3821815105759215616_n.jpg" alt=""/>
-								</div>
-								<div class="L2"><img class="image1" src="/assets/gallery/70122500_121337579244643_3821815105759215616_n.jpg" alt=""/>
-								</div>
-								<div class="S7"><img class="image1" src="/assets/gallery/70122500_121337579244643_3821815105759215616_n.jpg" alt=""/>
-								</div>
-								<div class="S8"><img class="image1" src="/assets/gallery/70122500_121337579244643_3821815105759215616_n.jpg" alt=""/>
-								</div>
-							</div>
-
+						<div class="L">
+							<img
+								class="image1"
+								src="/assets/gallery/69841845_131138014931266_2812637652514242560_n.jpg"
+								alt=""
+							/>
+						</div>
+						<div class="S1">
+							<img
+								class="image1"
+								src="/assets/gallery/70122500_121337579244643_3821815105759215616_n.jpg"
+								alt=""
+							/>
+						</div>
+						<div class="S2">
+							<img
+								class="image1"
+								src="/assets/gallery/70922147_131138091597925_598385121535459328_n.jpg"
+								alt=""
+							/>
+						</div>
+						<div class="S3">
+							<img
+								class="image1"
+								src="/assets/gallery/71813259_131137258264675_209588408270454784_n.jpg"
+								alt=""
+							/>
+						</div>
+						<div class="S4">
+							<img
+								class="image1"
+								src="/assets/gallery/71766217_131137268264674_3070247042149253120_n.jpg"
+								alt=""
+							/>
+						</div>
+						<div class="S5">
+							<img
+								class="image1"
+								src="/assets/gallery/71213662_131137248264676_505020924886515712_n.jpg"
+								alt=""
+							/>
+						</div>
+						<div class="S6">
+							<img
+								class="image1"
+								src="/assets/gallery/70122500_121337579244643_3821815105759215616_n.jpg"
+								alt=""
+							/>
+						</div>
+						<div class="L2">
+							<img
+								class="image1"
+								src="/assets/gallery/70122500_121337579244643_3821815105759215616_n.jpg"
+								alt=""
+							/>
+						</div>
+						<div class="S7">
+							<img
+								class="image1"
+								src="/assets/gallery/70122500_121337579244643_3821815105759215616_n.jpg"
+								alt=""
+							/>
+						</div>
+						<div class="S8">
+							<img
+								class="image1"
+								src="/assets/gallery/70122500_121337579244643_3821815105759215616_n.jpg"
+								alt=""
+							/>
+						</div>
+					</div>
 				</div>
 
 				{/* <div className="tagline-2">
 					<h2>lorem ipsum lorem ipsum lorem ipsum lorem ipsum</h2>
 					<h4 className="author">-lorem</h4>
 				</div> */}
-
 			</div>
 		</div>
 	);
