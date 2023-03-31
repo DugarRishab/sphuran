860aae07090a6d0d602768d4c496763c47e5265c
Rishab Dugar <www.anitadugar9163@gmail.com>
2023-03-31 15:49:33 +0530

    updated well-wishers style

--- client/src/pages/Wellwishers.jsx
+++ client/src/pages/Wellwishers.jsx
@@ -5,21 +5,34 @@ const WellWishers = () => {
 		<div className="well-wishers">
 			<div className="header">Our Well Wishers</div>
 			<div className="lower-body">
-				A Fest such as these could never Have
-				been possible without the support of many individuals behind the
-				scenes. <br></br>
+				A Fest such as these could never Have been possible without the
+				support of many individuals behind the scenes. <br></br>
+				
 				SPHURAN would like to thank all those who have been pivotal in
-				our success,some being:- <br></br>1. Mr. Gautam Ray, ED, HR ,CESC
-				<br></br>2. Mr. Sanjib Ray, PHE, WB govt. <br></br>3. Mr. Bikash
-				Manik, 1989 Batch, EE <br></br>4. Professor Konika Das
-				(Bhattacharya) , Head of Department, Department of Electrical
-				Engineering and all Faculty of Department of Electrical
-				Engineering, IIEST SHIBPUR <br></br>5. Mr. Joydeep
-				Sengupta, Father of Souvik Sengupta, 23' EE <br></br>6. Mr. Udayan
-				Chakrabarti, 1995, EE <br></br>7. Mr. Amlan Samanta, 1997, EE{" "}
-				<br></br>8. Miss Manami Talukdar, 2013, EE <br></br>9. Alumnis of
-				Batches 2022, 2021, 2020, 2019, 2018, 2017, 2016 for their
-				contribution And Many more...
+				our success, some being:-
+				<ul>
+					<li>Mr. Gautam Ray, ED, HR, CESC</li>
+					<li>Mr. Sanjib Ray, PHE, WB govt.</li>
+					<li>Mr. Bikash Manik, 1989 Batch, EE</li>
+					<li>
+						Professor Konika Das (Bhattacharya) , Head of
+						Department, Department of Electrical Engineering and all
+						Faculty of Department of Electrical Engineering, IIEST
+						SHIBPUR
+					</li>
+					<li>
+						Mr. Joydeep Sengupta, Father of Souvik Sengupta, 23'
+						EE
+					</li>
+					<li>Mr. Udayan Chakrabarti, 1995, EE</li>
+					<li>Mr. Amlan Samanta, 1997, EE</li>
+					<li>Miss Manami Talukdar, 2013, EE </li>
+					<li>
+						Alumnis of Batches 2022, 2021, 2020, 2019, 2018, 2017,
+						2016 for their contribution and Many more...
+					</li>
+				</ul>
+				
 			</div>
 		</div>
 	);
