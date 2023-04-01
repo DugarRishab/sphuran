579e8fdc907ff2dc0ddba9aba822af5c2262610d
Rishab Dugar <www.anitadugar9163@gmail.com>
2023-04-01 13:26:50 +0530

    updated event style

--- client/src/pages/Event.jsx
+++ client/src/pages/Event.jsx
@@ -17,12 +17,12 @@ const Event = ({user, login, logout}) => {
 			id: 2,
 			name: "Electroquip",
 			img: "",
-			desc: "Keen on quizzing yourselves with fluxes and farads, transformers and taps, lightning arcs and all things core? Wanna show off your calibre in the electrical domain? Well then, you are at just the right place. Organized in collaboration with industry experts, 'Electroquip' is a quiz event like no other. Get ready to test your concepts of basic electrical engineering, machines, power systems and control systems. Those who make it to the victory stand will get handsome prizes and suitable recognition for their knowledge and skills in the field.",
+			desc: <>Keen on quizzing yourselves with fluxes and farads, transformers and taps, lightning arcs and all things core? Wanna show off your calibre in the electrical domain? Well then, you are at just the right place. Organized in collaboration with industry experts, 'Electroquip' is a quiz event like no other. Get ready to test your concepts of basic electrical engineering, machines, power systems and control systems. Those who make it to the victory stand will get handsome prizes and suitable recognition for their knowledge and skills in the field.</>,
 			rounds: "3",
-			venue: "Round 1- 7/4,1-1:45,I hall <br></br> Round 2- 8/4,10:30-11:30,EE seminar hall <br> </br> Round 3-9/4,10 AM- 11 AM,EE seminar hall",
+			venue: <>Round 1- I hall <br></br> Round 2- EE seminar hall <br></br> Round 3- EE seminar hall</>,
 			teamSize: "2 to 3",
-			prizeMoney: "₹12,000",
-			entryFee: "₹70/team",
+			prizeMoney: "12,000",
+			entryFee: "70/team",
 			rules: "https://drive.google.com/drive/folders/1yF4Bdg4OLutiZk-ClbdwSn_Y9qAoPLV2",
 		},
 
@@ -30,23 +30,23 @@ const Event = ({user, login, logout}) => {
 			id: 3,
 			name: "Illumination ",
 			img: "",
-			desc: "Attention all electronics enthusiasts!   <br></br> Here's your chance to showcase your electronic skills! A series of challenging design problems awaits you at “Illumination”, a circuit simulation event. Your aim is to engineer the best electronic circuits that solve the problems, using any circuit simulation software of your choice. Credit will be given based on the functionality, ingenuity and efficiency of the design. We also have a hardware round, where you will be working with real components and breathing life into your designs.  <br></br> Have fun creating new circuits and watching the electrons dance around!",
+			desc: <p>Attention all electronics enthusiasts!   <br></br> Here's your chance to showcase your electronic skills! A series of challenging design problems awaits you at “Illumination”, a circuit simulation event. Your aim is to engineer the best electronic circuits that solve the problems, using any circuit simulation software of your choice. Credit will be given based on the functionality, ingenuity and efficiency of the design. We also have a hardware round, where you will be working with real components and breathing life into your designs.  <br></br> Have fun creating new circuits and watching the electrons dance around!</p>,
 			rounds: "2",
 			venue: "Round 1 & 2- S-464 (3rd Floor)",
 			teamSize: "1 to 2",
-			prizeMoney: "₹10,000",
-			entryFee: "₹50/team",
+			prizeMoney: "10,000",
+			entryFee: "50/team",
 			rules: "https://drive.google.com/drive/folders/1yF4Bdg4OLutiZk-ClbdwSn_Y9qAoPLV2",
 		},
 		{
 			id: 4,
 			name: "Cyber Blame",
 			img: "",
-			desc: "From smartphones to self-driving cars, technology is changing the way we live, work, and communicate with each other. And at the forefront of this technological revolution are coders – the people who create the software that powers our devices and systems.<br></br> To celebrate the importance of coding and to encourage the development of new and innovative technology, we are thrilled to announce Cyber Blame, a coding contest for our upcoming tech fest, Sphuran 2023.  <br></br> The coding contest is the perfect opportunity to put your skills and knowledge of programming languages to the test and compete against other talented coders. <br> </br> So, dust off your programming skills, fire up your laptop, and get ready to take on the challenge!",
+			desc: <>From smartphones to self-driving cars, technology is changing the way we live, work, and communicate with each other. And at the forefront of this technological revolution are coders - the people who create the software that powers our devices and systems.<br></br> To celebrate the importance of coding and to encourage the development of new and innovative technology, we are thrilled to announce Cyber Blame, a coding contest for our upcoming tech fest, Sphuran 2023.  <br></br> The coding contest is the perfect opportunity to put your skills and knowledge of programming languages to the test and compete against other talented coders. <br> </br> So, dust off your programming skills, fire up your laptop, and get ready to take on the challenge!</>,
 			rounds: "1",
 			venue: "Online",
 			teamSize: "1",
-			prizeMoney: "₹10,000",
+			prizeMoney: "10,000",
 			entryFee: "70/team",
 			rules: "https://drive.google.com/drive/folders/1yF4Bdg4OLutiZk-ClbdwSn_Y9qAoPLV2",
 		},
@@ -54,12 +54,12 @@ const Event = ({user, login, logout}) => {
 			id: 5,
 			name: "Argumentation",
 			img: "",
-			desc: "It's going to be legen-- wait for it --dary. It's going to be legendary! <br></br>Equipped with facts and logic along with the desire to excel in heated arguments, the contenders are ready for the battle of words to begin in `Argumentation`. Powerful speeches, fiery replies and definitive comebacks. Team Sphuran is excited to conduct this debate as it is sure to rouse your oratory skills letting you exhibit the power of your mind in an electrifying debate. <br></br>So hold no vendetta as we seek flaming debates with red-hot words! Meet you at the disclosure. <br> <br> Happy debating!",
+			desc: <>It's going to be legen-- wait for it --dary. It's going to be legendary! <br></br>Equipped with facts and logic along with the desire to excel in heated arguments, the contenders are ready for the battle of words to begin in `Argumentation`. Powerful speeches, fiery replies and definitive comebacks. Team Sphuran is excited to conduct this debate as it is sure to rouse your oratory skills letting you exhibit the power of your mind in an electrifying debate. <br></br>So hold no vendetta as we seek flaming debates with red-hot words! Meet you at the disclosure. <br> </br> Happy debating!</>,
 			rounds: "1",
 			venue: "EE Seminar Hall",
 			teamSize: "2",
-			prizeMoney: "₹8,000",
-			entryFee: "₹70/team",
+			prizeMoney: "8,000",
+			entryFee: "70/team",
 			rules: "https://drive.google.com/drive/folders/1yF4Bdg4OLutiZk-ClbdwSn_Y9qAoPLV2",
 		},
 		// {
@@ -73,12 +73,12 @@ const Event = ({user, login, logout}) => {
 			id: 7,
 			name: "Adhayan",
 			img: "",
-			desc: "Attention all tech enthusiasts!  <br></br>Are you ready to showcase your problem-solving skills and analytical thinking in the tech industry? If so, we have an exciting opportunity for you! <br></br> Sphuran 2023 includes Adhayan, a case study competition that challenges participants to come up with innovative solutions to real-world current tech trends and business problems. The competition apart from boosting your creative thinking would provide some amazing prizes for the winners, including cash prizes, and certificates of recognition. <br></br> We can't wait to see you at the Tech Fest and witness the incredible solutions you come up with. Let's innovate, analyze, and excel together in the world of tech!",
+			desc: <>Attention all tech enthusiasts!  <br></br>Are you ready to showcase your problem-solving skills and analytical thinking in the tech industry? If so, we have an exciting opportunity for you! <br></br> Sphuran 2023 includes Adhayan, a case study competition that challenges participants to come up with innovative solutions to real-world current tech trends and business problems. The competition apart from boosting your creative thinking would provide some amazing prizes for the winners, including cash prizes, and certificates of recognition. <br></br> We can't wait to see you at the Tech Fest and witness the incredible solutions you come up with. Let's innovate, analyze, and excel together in the world of tech!</>,
 			rounds: "2",
-			venue: "ROund 1 - Online <br></br> Round 2 - EE SEminar Hall",
+			venue: <>Round 1 - Online <br></br> Round 2 - EE SEminar Hall</>,
 			teamSize: "2 to 3",
-			prizeMoney: "₹10,000",
-			entryFee: "₹50/team",
+			prizeMoney: "10,000",
+			entryFee: "50/team",
 			rules: "https://drive.google.com/drive/folders/1yF4Bdg4OLutiZk-ClbdwSn_Y9qAoPLV2",
 		},
 		// {
@@ -98,7 +98,7 @@ const Event = ({user, login, logout}) => {
 			id: 10,
 			name: "voltage voyage",
 			img: "",
-			desc: "Do you ever dream of being on a mind-bending adventure spanning several exciting landscapes in an Indiana Jones-ish style? Well, Sphuran 2.0 brings you another breathtaking event  Voltage Voyage where you need to rush your brain all way through the basic concepts of Electrical Engineering to track down the hidden treasures and snatch the chance of squashing out the best of team work. <bg></bg> So guys, energise yourself and get ready to manifest something so educative yet so much fun !",
+			desc: <>Do you ever dream of being on a mind-bending adventure spanning several exciting landscapes in an Indiana Jones-ish style? Well, Sphuran 2.0 brings you another breathtaking event  Voltage Voyage where you need to rush your brain all way through the basic concepts of Electrical Engineering to track down the hidden treasures and snatch the chance of squashing out the best of team work. <bg></bg> So guys, energise yourself and get ready to manifest something so educative yet so much fun !</>,
 			rules: "https://drive.google.com/drive/folders/1yF4Bdg4OLutiZk-ClbdwSn_Y9qAoPLV2",
 			rounds: "1",
 			venue: "Parade Ground",
@@ -110,7 +110,7 @@ const Event = ({user, login, logout}) => {
 			id: 11,
 			name: "Ace the Race",
 			img: "",
-			desc: "Love building new and advanced robots? Wanna test your bot deign skills to the limit? <br></br>“Ace the Race” is an exciting robot design competition. We envision that student teams will develop their own robotic systems in order to compete in Line follower races, challenging the limits of robotic design and autonomy technologies. This challenge will test the speed, accuracy and agility of your robots to the next level. So get ready to amp your design skills for a fierce robo battle!",
+			desc: <>Love building new and advanced robots? Wanna test your bot deign skills to the limit? <br></br>“Ace the Race” is an exciting robot design competition. We envision that student teams will develop their own robotic systems in order to compete in Line follower races, challenging the limits of robotic design and autonomy technologies. This challenge will test the speed, accuracy and agility of your robots to the next level. So get ready to amp your design skills for a fierce robo battle!</>,
 			rules: "https://drive.google.com/drive/folders/1yF4Bdg4OLutiZk-ClbdwSn_Y9qAoPLV2",
 			rounds: "1",
 			venue: "Netaji Bhawan",
@@ -122,10 +122,10 @@ const Event = ({user, login, logout}) => {
 			id: 12,
 			name: "Tech Trends",
 			img: "",
-			desc: "Don't we often lose our way through tedious chapters of unappealing books while going for something quite simple and conclusive? As far as we go, that's definitely a very boring way to learn.<br></br>Therefore, be your own mentor to learning as we are yet with another unique and exciting event where you can showcase your hidden talent of presenting ideas in a visually simpler way. All you need to do is, bring up your research recipe, cook it up with some attractive content, garnish it with lots, and lots of your creativity with a pinch of simplicity, and there you go, ready to serve! <br></br>It's always nice to find your hidden self, isn't it? So what are you even waiting for, go and register now!",
+			desc: <>Don't we often lose our way through tedious chapters of unappealing books while going for something quite simple and conclusive? As far as we go, that's definitely a very boring way to learn.<br></br>Therefore, be your own mentor to learning as we are yet with another unique and exciting event where you can showcase your hidden talent of presenting ideas in a visually simpler way. All you need to do is, bring up your research recipe, cook it up with some attractive content, garnish it with lots, and lots of your creativity with a pinch of simplicity, and there you go, ready to serve! <br></br>It's always nice to find your hidden self, isn't it? So what are you even waiting for, go and register now!</>,
 			rules: "https://drive.google.com/drive/folders/1yF4Bdg4OLutiZk-ClbdwSn_Y9qAoPLV2",
 			rounds: "2",
-			venue: "Round 1 - EE sublibrary <br></br> Round 2 - EE Sublibrary",
+			venue: <>Round 1 - EE sublibrary <br></br> Round 2 - EE Sublibrary</>,
 			teamSize: "2 to 3",
 			prizeMoney: "10,000",
 			entryFee: "70/team",
@@ -134,7 +134,7 @@ const Event = ({user, login, logout}) => {
 			id: 13,
 			name: "Shatranj",
 			img: "",
-			desc: "Traxler , Scandinavian, Sicilian , Stafford or Ruy Lopez....<br></br>Which one do you prefer or wanna try your own variations this time ? <br></br> Delve in your thoughts ,put together your openings and defences , set up your traps and prepare yourself for the next Gambit cause <br></br>Sphuran 2023 brings to you an enthusing chess competition  on  7/04/23.     And don't forget ... <br/br>' Every move must have a purpose ! !'",
+			desc: <>Traxler , Scandinavian, Sicilian , Stafford or Ruy Lopez....<br></br>Which one do you prefer or wanna try your own variations this time ? <br></br> Delve in your thoughts ,put together your openings and defences , set up your traps and prepare yourself for the next Gambit cause <br></br>Sphuran 2023 brings to you an enthusing chess competition  on  7/04/23.     And don't forget ... <br></br>' Every move must have a purpose ! !'</>,
 			rules: "https://drive.google.com/drive/folders/1yF4Bdg4OLutiZk-ClbdwSn_Y9qAoPLV2",
 			rounds: "1",
 			venue: "Amenities",
@@ -146,7 +146,7 @@ const Event = ({user, login, logout}) => {
 			id: 14,
 			name: "Fun Events",
 			img: "",
-			desc: "Are you tired of only technical events? No worries, we heard you. We've got an exciting lineup of games that will put your skills to the test and keep you entertained throughout the day. <br></br>First up is the Dart Game, where you can show off your precision and accuracy by hitting the bullseye and scoring high. <br></br> Next, we have Wire the Loop. This exciting game involves taking a ring of wire through a loop without touching it. It may sound simple, but it requires a steady hand, careful planning, and quick reflexes. <br></br> And finally, get ready for the Headphone Game, where you'll wear noise-canceling headphones and try to lip-read what the coordinator is saying, competing against others to see who can get the most phrases right. <br></br> Don't miss out on this opportunity to challenge your technical skills and have fun at the same time!",
+			desc: <>Are you tired of only technical events? No worries, we heard you. We've got an exciting lineup of games that will put your skills to the test and keep you entertained throughout the day. <br></br>First up is the Dart Game, where you can show off your precision and accuracy by hitting the bullseye and scoring high. <br></br> Next, we have Wire the Loop. This exciting game involves taking a ring of wire through a loop without touching it. It may sound simple, but it requires a steady hand, careful planning, and quick reflexes. <br></br> And finally, get ready for the Headphone Game, where you'll wear noise-canceling headphones and try to lip-read what the coordinator is saying, competing against others to see who can get the most phrases right. <br></br> Don't miss out on this opportunity to challenge your technical skills and have fun at the same time!</>,
 			rules: "https://drive.google.com/drive/folders/1yF4Bdg4OLutiZk-ClbdwSn_Y9qAoPLV2",
 			rounds: "1",
 			venue: "Parade Ground",
@@ -158,10 +158,10 @@ const Event = ({user, login, logout}) => {
 			id: "15",
 			name: "Vidyut Gyan",
 			img: "",
-			desc: "Attention all electrical enthusiasts! It's time to put your knowledge to the test in our upcoming electrical quiz competition! <br> </br> This competition is open to anyone with a passion for electrical engineering knowledge and a desire to learn more about the field. Whether you're a student, a professional, or just an enthusiast, this is your chance to showcase your knowledge and compete with other like-minded individuals.",
+			desc: <>Attention all electrical enthusiasts! It's time to put your knowledge to the test in our upcoming electrical quiz competition! <br> </br> This competition is open to anyone with a passion for electrical engineering knowledge and a desire to learn more about the field. Whether you're a student, a professional, or just an enthusiast, this is your chance to showcase your knowledge and compete with other like-minded individuals.</>,
 			rules: "https://drive.google.com/drive/folders/1yF4Bdg4OLutiZk-ClbdwSn_Y9qAoPLV2",
 			rounds: "3",
-			venue: "Round 1 - ALumni Seminar Hall <br></br> S-470 (3rd Floor)",
+			venue: <>Round 1 - ALumni Seminar Hall <br></br> S-470 (3rd Floor)</>,
 			teamSize: "1-2",
 			prizeMoney: "8000",
 			entryFee: "50/team",
@@ -213,17 +213,39 @@ const Event = ({user, login, logout}) => {
 							<div className="desc-body">
 								{event.desc}
 								<div className="details">
-									<div>Rounds - {event.rounds}</div>
+									<div>
+										<b>Rules</b> - <a href={event.rules}>{event.rules}</a>
+									</div><br></br>
+									<div>
+										<b>Rounds</b> - {event.rounds}
+									</div>
 									{/* <div>Time - {event.time}</div>
 									<div>Venue - {event.venue}</div> */}
+									<div>
+										<b>Venue</b> - {event.venue}
+									</div>
+									<div>
+										<b>Team Size</b> - {event.teamSize}
+									</div>
+									<div>
+										<b>Prize Money</b> - ₹{event.prizeMoney}
+									</div>
+									<div>
+										<b>Entry Fee</b> - ₹{event.entryFee}
+									</div>
 								</div>
-								{user && !user.events.includes(event.id.toString()) && (
-									<CustomButton
-										onClick={() => handleRegister(event.id)}
-										variant={"contained"}
-										text="Register Now"
-									></CustomButton>
-								)}
+								{user &&
+									!user.events.includes(
+										event.id.toString()
+									) && (
+										<CustomButton
+											onClick={() =>
+												handleRegister(event.id)
+											}
+											variant={"contained"}
+											text="Register Now"
+										></CustomButton>
+									)}
 							</div>
 						</div>
 					</div>
