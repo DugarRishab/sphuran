81f4eec80e55956d1655583cb54d5f50fe2093ad
Rishab Dugar <www.anitadugar9163@gmail.com>
2024-04-07 11:10:01 +0530

    [UPDATE]: updated scheudule layout

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -58,6 +58,14 @@ const Events = ({ user, login, logout }) => {
 		// 		</>
 		// 	),
 		// },
+		// {
+		// 	id: 1,
+		// 	name: "Inaugration + Guest Speaker",
+		// 	img: "",
+		// 	desc: "",
+		// 	rounds: "1",
+		// 	venue: "I-Hall",
+		// },
 		{
 			id: "2",
 			name: "Electroquip",
@@ -79,8 +87,7 @@ const Events = ({ user, login, logout }) => {
 			rounds: "3",
 			venue: (
 				<>
-					Round 1- I hall <br></br> Round 2- EE seminar hall <br></br>{" "}
-					Round 3- EE seminar hall
+					Round 1, 2, & 3 - EE seminar hall (2nd Floor, EE Dept)
 				</>
 			),
 			teamSize: "2 to 3",
@@ -324,36 +331,36 @@ const Events = ({ user, login, logout }) => {
 			prizeMoney: "4,000",
 			entryFee: "50/team (for non-iiestians)",
 		},
-		{
-			id: "14",
-			name: "Fun Events",
-			img: "/assets/eventposters/funevents.jpeg",
-			desc: (
-				<>
-					Are you tired of only technical events? No worries, we heard
-					you. We've got an exciting lineup of games that will put
-					your skills to the test and keep you entertained throughout
-					the day. <br></br>First up is the Dart Game, where you can
-					show off your precision and accuracy by hitting the bullseye
-					and scoring high. <br></br> Next, we have Wire the Loop.
-					This exciting game involves taking a ring of wire through a
-					loop without touching it. It may sound simple, but it
-					requires a steady hand, careful planning, and quick
-					reflexes. <br></br> And finally, get ready for the Headphone
-					Game, where you'll wear noise-canceling headphones and try
-					to lip-read what the coordinator is saying, competing
-					against others to see who can get the most phrases right.{" "}
-					<br></br> Don't miss out on this opportunity to challenge
-					your technical skills and have fun at the same time!
-				</>
-			),
-			rules: "https://drive.google.com/drive/folders/1yF4Bdg4OLutiZk-ClbdwSn_Y9qAoPLV2",
-			rounds: "1",
-			venue: "Parade Ground",
-			teamSize: "1",
-			prizeMoney: "Goodies for All",
-			entryFee: "20/team (for all)",
-		},
+		// {
+		// 	id: "14",
+		// 	name: "Fun Events",
+		// 	img: "/assets/eventposters/funevents.jpeg",
+		// 	desc: (
+		// 		<>
+		// 			Are you tired of only technical events? No worries, we heard
+		// 			you. We've got an exciting lineup of games that will put
+		// 			your skills to the test and keep you entertained throughout
+		// 			the day. <br></br>First up is the Dart Game, where you can
+		// 			show off your precision and accuracy by hitting the bullseye
+		// 			and scoring high. <br></br> Next, we have Wire the Loop.
+		// 			This exciting game involves taking a ring of wire through a
+		// 			loop without touching it. It may sound simple, but it
+		// 			requires a steady hand, careful planning, and quick
+		// 			reflexes. <br></br> And finally, get ready for the Headphone
+		// 			Game, where you'll wear noise-canceling headphones and try
+		// 			to lip-read what the coordinator is saying, competing
+		// 			against others to see who can get the most phrases right.{" "}
+		// 			<br></br> Don't miss out on this opportunity to challenge
+		// 			your technical skills and have fun at the same time!
+		// 		</>
+		// 	),
+		// 	rules: "https://drive.google.com/drive/folders/1yF4Bdg4OLutiZk-ClbdwSn_Y9qAoPLV2",
+		// 	rounds: "1",
+		// 	venue: "Parade Ground",
+		// 	teamSize: "1",
+		// 	prizeMoney: "Goodies for All",
+		// 	entryFee: "20/team (for all)",
+		// },
 		{
 			id: "15",
 			name: "Vidyut Gyan",
@@ -381,34 +388,34 @@ const Events = ({ user, login, logout }) => {
 			prizeMoney: "8000",
 			entryFee: "50/team (for non-iiestians)",
 		},
-		{
-			id: "16",
-			name: "Alumni Convention",
-			img: "/assets/eventposters/Alumni Convention.png",
-			desc: (
-				<>
-					As the fruit never falls away from a tree,one can never be
-					away from their Alma mater no matter where they are in the
-					World.
-					<br></br> For the first time,Sphuran 2.0 is organising an
-					alumni meet for all those who came ahead of us. All are
-					invited to grace the occasion and bask in the glory days of
-					their college life within the hallowed walls of the
-					Department.
-					<br></br> Further, our alumnis who are in many positions
-					around the world would grace us with their wisdom. A
-					Business meet would follow the alumni convention. Come all
-					and listen to experiences of those who have sailed the sea
-					of the professional world.
-				</>
-			),
-			rules: "",
-			rounds: "",
-			venue: <>EE Seminar Hall, 9th April, 4PM - 6PM</>,
-			/*teamSize: "1-2",
-			prizeMoney: "8000",
-			entryFee: "50/team",*/
-		},
+		// {
+		// 	id: "16",
+		// 	name: "Alumni Convention",
+		// 	img: "/assets/eventposters/Alumni Convention.png",
+		// 	desc: (
+		// 		<>
+		// 			As the fruit never falls away from a tree,one can never be
+		// 			away from their Alma mater no matter where they are in the
+		// 			World.
+		// 			<br></br> For the first time,Sphuran 2.0 is organising an
+		// 			alumni meet for all those who came ahead of us. All are
+		// 			invited to grace the occasion and bask in the glory days of
+		// 			their college life within the hallowed walls of the
+		// 			Department.
+		// 			<br></br> Further, our alumnis who are in many positions
+		// 			around the world would grace us with their wisdom. A
+		// 			Business meet would follow the alumni convention. Come all
+		// 			and listen to experiences of those who have sailed the sea
+		// 			of the professional world.
+		// 		</>
+		// 	),
+		// 	rules: "",
+		// 	rounds: "",
+		// 	venue: <>EE Seminar Hall, 9th April, 4PM - 6PM</>,
+		// 	/*teamSize: "1-2",
+		// 	prizeMoney: "8000",
+		// 	entryFee: "50/team",*/
+		// },
 		{
 			id: "17",
 			name: "Online Gaming",
@@ -431,15 +438,15 @@ const Events = ({ user, login, logout }) => {
 			rounds: "As many required",
 			venue: (
 				<>
-					Valorant- 7/4,9:30 PM onwards,Online
+					BGMI- 7th and 8th, 10:00 PM onwards, Online
 					<br />
-					Fifa- 8/4,7:30 PM-9:30 PM, Amenities
+					Fifa- 8th, 9:00 PM - 10:00 PM, Amenities
 				</>
 			),
 			teamSize: (
 				<>
 					Fifa-1 <br></br>
-					Valo-5+1
+					BGMI-5+1
 				</>
 			),
 			prizeMoney: "4,000/Game",
@@ -447,7 +454,7 @@ const Events = ({ user, login, logout }) => {
 				<>
 					Fifa-40 (For All)
 					<br />
-					Valo-100/Team (For All){" "}
+					BGMI-100/Team (For All){" "}
 				</>
 			),
 		},
@@ -455,102 +462,84 @@ const Events = ({ user, login, logout }) => {
 
 	const Day1Events = [
 		{
-			name: "Inauguration and Industrial Conclave ",
-			time: "10am to 12pm",
+			name: "Inauguration and Guest Speaker",
+			time: "10am to 12:30pm",
 			venue: "I-hall",
 		},
 		{
-			name: "Electroquip",
-			time: "1pm to 1:45pm",
-			venue: "I-hall",
+			name: "Electroquip (Round 1)",
+			time: "1pm to 2pm",
+			venue: "EE-Seminar Hall",
 		},
 		{
-			name: "Tech Trends (Round 1)",
-			time: "2pm to 3pm",
-			venue: "EE Sublibrary",
+			name: "Illuminaiton (Round 1)",
+			time: "3pm to 4pm",
+			venue: "Room S464, 3rd Floor, EE Dept.",
 		},
 		{
-			name: "Argumentation",
-			time: "3:30pm to 5:30pm",
-			venue: "EE Seminar Hall",
-		},
-		{
-			name: "Chess",
-			time: "6pm to 7:30pm",
+			name: "Shatranj",
+			time: "4pm to 6pm ",
 			venue: "Amenities",
 		},
 		{
-			name: "CyberBlame",
-			time: "8pm to 9:30pm",
-			venue: "Online (From Hostel)",
+			name: "Argumentation",
+			time: "6pm to 8:30pm",
+			venue: "EE Seminar Hall",
 		},
+
 		{
-			name: "Online Games (Valorant)",
-			time: "From 9:30pm",
+			name: "BGMI",
+			time: "10pm onwards",
 			venue: "Online (From Hostel)",
 		},
+		
 	];
 	const Day2Events = [
 		{
 			name: "Electroquip (Round 2)",
-			time: "10:30am to 11:30am",
+			time: "5pm to 6pm",
 			venue: "EE seminar Hall",
 		},
 		{
-			name: "Vidyut Gyan (Round 1)",
-			time: "12pm to 1pm",
+			name: "Vidyut Gyan",
+			time: "6pm to 7:30pm",
 			venue: "Alumni Seminar Hall",
 		},
 		{
-			name: "Adhyayan (Round 2)",
-			time: "3pm to 4pm",
+			name: "Illumination (Round 2)",
+			time: "7:30pm to 9pm",
 			venue: "EE seminar Hall",
 		},
 		{
-			name: "Voltage Voyage",
-			time: "4pm to 6:30pm",
-			venue: "Parade Ground",
+			name: "CYber Blame",
+			time: "9pm to 11pm",
+			venue: "Online",
 		},
 		{
-			name: "Online Games (Fifa)",
+			name: "FIFA (all rounds)",
 			time: "7:30pm to 9:30pm",
 			venue: "Amenities",
 		},
+		{
+			name: "BGMI",
+			time: "10pm onwards",
+			venue: "Online",
+		},
 	];
 	const Day3Events = [
+		{
+			name: "Adhyayan (Round 2)",
+			time: "5pm to 6pm",
+			venue: "EE Sub Library",
+		},
 		{
 			name: "Electroquip (Round 3)",
 			time: "10am to 11am",
 			venue: "EE Seminar Hall",
 		},
 		{
-			name: "Vidyut Gyan (Round 2)",
-			time: "11:30am to 1pm",
-			venue: "S-470 (3rd Floor)",
-		},
-		{
-			name: "Illumination (Round 2)",
-			time: "1:30pm to 2:30pm",
-			venue: "S-464 (3rd Floor)",
-		},
-		{
-			name: "Tech trends (Round 2)",
-			time: "2:30pm to 3:30pm",
-			venue: "EE Sublibrary",
-		},
-		{
-			name: "Ace the race",
-			time: "3pm to 4pm",
-			venue: "Netaji Bhawan",
-		},
-		{
-			name: "Alumni convention",
-			time: "4pm to 6pm",
-			venue: "EE Seminar Hall",
-		},
-		{
-			name: "Valedictory Ceremony",
-			time: "From 6pm",
+			name: "CLosing Ceremony",
+			time: "From 7:30pm",
 			venue: "I Hall",
 		},
 	];
